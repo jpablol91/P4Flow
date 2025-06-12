3744b40ced1e0fef8c465b2028a76c843b102fdf
Juan Pablo López Martínez <jpablol91@gmail.com>
2025-06-12 04:47:01 +0000

    topo3

--- code/P4/topo3.py
+++ code/P4/topo3.py
@@ -1,18 +1,4 @@
-#!/usr/bin/python
-
-# Copyright 2013-present Barefoot Networks, Inc. 
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/env python3
 
 from mininet.net import Mininet
 from mininet.topo import Topo
@@ -24,9 +10,8 @@ from p4_mininet import P4Switch, P4Host
 
 import argparse
 from time import sleep
-import os, time, re
+import os
 import subprocess
-
 
 _THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 _THRIFT_BASE_PORT = 9090
@@ -43,60 +28,50 @@ parser.add_argument('--size', help='UDP packet size',
 
 args = parser.parse_args()
 
-
-args.json="P4Flow.json"
-args.behavioral_exe="simple_switch"
-args.cli="simple_switch_CLI"
+# Hardcoded fallback (overwrite parsed args if not provided)
+args.json = args.json or "P4Flow.json"
+args.behavioral_exe = args.behavioral_exe or "simple_switch"
+args.cli = args.cli or "simple_switch_CLI"
 
 class MyTopo(Topo):
     def __init__(self, sw_path, json_path, nb_hosts, nb_switches, links, **opts):
-        # Initialize topology and default options
-        print('opts',opts)
-        Topo.__init__(self, **opts)
+        super(MyTopo, self).__init__(**opts)
 
-        for i in xrange(nb_switches):
-           	switch = self.addSwitch('s%d' % (i + 1),
-                                    sw_path = sw_path,
-                                    json_path = args.json,
-                                    thrift_port = _THRIFT_BASE_PORT + i,
-                                    log_console = False,
-                                    pcap_dump = False,
-                                    device_id = i)
+        for i in range(nb_switches):
+            self.addSwitch(f's{i+1}',
+                           sw_path=sw_path,
+                           json_path=json_path,
+                           thrift_port=_THRIFT_BASE_PORT + i,
+                           log_console=False,
+                           pcap_dump=False,
+                           device_id=i)
 
-        
-        for h in xrange(nb_hosts):
-            #host_ip = "10.0.%d.%d" % (nb_switches, h+1)
-            host_ip = "10.0.%d.%d/24" % (h+1, h+1)
-            print('host_ip',host_ip)
-            #host_mac = '00:00:00:00:%02x:%02x' % (nb_switches, h+1)
-            host_mac = '00:00:00:00:%02x:%02x' % (h+1, h+1)
-            print('host_mac',host_mac)
-            host = self.addHost('h%d' % (h + 1),ip=host_ip,mac=host_mac)
+        for h in range(nb_hosts):
+            host_ip = f"10.0.{h+1}.{h+1}/24"
+            host_mac = f'00:00:00:00:{h+1:02x}:{h+1:02x}'
+            self.addHost(f'h{h+1}', ip=host_ip, mac=host_mac)
 
         for a, b in links:
-            #delay_key = ''.join([host_name, sw])
-            #delay = latencies[delay_key] if delay_key in latencies else '0ms'
-            #bw = bws[delay_key] if delay_key in bws else None
             self.addLink(a, b)
-            
 
 def read_topo():
     nb_hosts = 0
     nb_switches = 0
     links = []
     with open("topo.txt", "r") as f:
-        line = f.readline()[:-1]
+        line = f.readline().strip()
         w, nb_switches = line.split()
-        assert(w == "switches")
-        line = f.readline()[:-1]
+        assert w == "switches"
+        line = f.readline().strip()
         w, nb_hosts = line.split()
-        assert(w == "hosts")
+        assert w == "hosts"
         for line in f:
-            if not f: break
+            line = line.strip()
+            if not line:
+                continue
             a, b = line.split()
-            links.append( (a, b) )
+            links.append((a, b))
     return int(nb_hosts), int(nb_switches), links
-    
 
 def main():
     nb_hosts, nb_switches, links = read_topo()
@@ -105,72 +80,61 @@ def main():
                   args.json,
                   nb_hosts, nb_switches, links)
 
-    net = Mininet(topo = topo,
-                  host = P4Host,
-                  switch = P4Switch,
-                  link = TCLink,
-                  controller = None )
+    net = Mininet(topo=topo,
+                  host=P4Host,
+                  switch=P4Switch,
+                  link=TCLink,
+                  controller=None)
     net.start()
 
     for host_name in topo.hosts():
-            h = net.get(host_name)
-            h_iface = h.intfs.values()[0]
-            link = h_iface.link
+        h = net.get(host_name)
+        h_iface = list(h.intfs.values())[0]
+        link = h_iface.link
 
-            sw_iface = link.intf1 if link.intf1 != h_iface else link.intf2
-            # phony IP to lie to the host about
-            host_id = int(host_name[1:])
-            sw_ip = '10.0.%d.254' % host_id
+        sw_iface = link.intf1 if link.intf1 != h_iface else link.intf2
+        host_id = int(host_name[1:])
+        sw_ip = f'10.0.{host_id}.254'
 
-            # Ensure each host's interface name is unique, or else
-            # mininet cannot shutdown gracefully
-            h.defaultIntf().rename('%s-eth0' % host_name)
-            # static arp entries and default routes
-            h.cmd('arp -i %s -s %s %s' % (h_iface.name, sw_ip, sw_iface.mac))
-            h.cmd('ethtool --offload %s rx off tx off' % h_iface.name)
-            h.cmd('ip route add %s dev %s' % (sw_ip, h_iface.name))
-            h.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
-            h.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
-            h.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
-            #h.cmd("sysctl -w net.ipv4.tcp_congestion_control=reno")
-            h.cmd("iptables -I OUTPUT -p icmp --icmp-type destination-unreachable -j DROP")
-
-            h.setDefaultRoute("via %s" % sw_ip)
+        h.defaultIntf().rename(f'{host_name}-eth0')
+        h.cmd(f'arp -i {h_iface.name} -s {sw_ip} {sw_iface.mac}')
+        h.cmd(f'ethtool --offload {h_iface.name} rx off tx off')
+        h.cmd(f'ip route add {sw_ip} dev {h_iface.name}')
+        h.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
+        h.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
+        h.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
+        h.cmd("iptables -I OUTPUT -p icmp --icmp-type destination-unreachable -j DROP")
+        h.setDefaultRoute(f"via {sw_ip}")
 
     sleep(1)
 
-    for i in xrange(nb_switches):
-        s = net.get('s%d' % (i + 1))
-        print "##################################\n"
-        print "Switch (s%d)" % (i + 1)
-        result = s.cmd('ifconfig')
-        print	result
-        print "MAC Address: \t%s" 	% s.MAC()
-        print "IP Address: \t%s\n" 	% s.IP()
-        print "##################################"
-      	cmd = [args.cli, "--json", args.json,"--thrift-port", str(_THRIFT_BASE_PORT + i)]
-        with open("s%d-commands.txt"%(i + 1), "r") as f:
-            print " ".join(cmd)
-            try:
-                output = subprocess.check_output(cmd, stdin = f)
-                print output
-            except subprocess.CalledProcessError as e:
-                print e
-                print e.output
+    for i in range(nb_switches):
+        s = net.get(f's{i+1}')
+        print("##################################")
+        print(f"Switch (s{i+1})")
+        print(s.cmd('ifconfig'))
+        print(f"MAC Address:\t{s.MAC()}")
+        print(f"IP Address:\t{s.IP()}")
+        print("##################################")
+
+        cmd = [args.cli, "--json", args.json, "--thrift-port", str(_THRIFT_BASE_PORT + i)]
+        cli_input = f"s{i+1}-commands.txt"
+
+        if os.path.exists(cli_input):
+            with open(cli_input, "r") as f:
+                print(" ".join(cmd))
+                try:
+                    output = subprocess.check_output(cmd, stdin=f, text=True)
+                    print(output)
+                except subprocess.CalledProcessError as e:
+                    print(e)
+                    print(e.output)
 
     sleep(1)
-
-    print "Ready !"
-
-    ## do test for link failure
-    #G = nx.read_graphml('AttMpls.graphml')
-    #nx.draw(G, with_labels=True)
-    h1, h2 = net.getNodeByName('h1', 'h2')
-    
-  
-    CLI( net )
+    print("Ready!")
+    CLI(net)
     net.stop()
 
 if __name__ == '__main__':
-    setLogLevel( 'info' )
+    setLogLevel('info')
     main()
